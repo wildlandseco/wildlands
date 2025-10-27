ff2a10ca12fb1195ede3393af48e9b63e9f05342
wildlandseco <soapjohnson@icloud.com>
2025-10-26 23:42:40 -0500

    Add pages/index.tsx fallback routine

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Wild Lands – Home</h1>
+      <p>If you see this, routing works. The App Router page will be easy to re-enable.</p>
+    </main>
+  );
+}
